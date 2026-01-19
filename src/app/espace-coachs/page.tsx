82b1118d73de56a73254c12bb5114a87e9fc3675
Noctis-19 <maxime.verger.pro@gmail.com>
2026-01-19 13:52:05 +0100

    Improved page for espaceCoachs

--- src/app/espace-coachs/page.tsx
+++ src/app/espace-coachs/page.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image';
+
 export default function EspaceCoachsPage() {
   return (
-    <main>
-      <h1>Espace coachs</h1>
-      <p>Page visible après connexion (ou test sans protection)</p>
-    </main>
+    <div className="max-w-7xl mx-auto px-4 py-6 text-lg text-justify">
+      <h2 className="text-4xl text-primary font-bold mb-6">Espace coachs</h2>
+
+      <p>Bienvenue dans l'espace à destination des coachs.</p>
+      <p>Vous avez accès aux mêmes pages que les autres utilisateurs mais avec une messagerie uniquement visible pour les coachs dans les onglets évènements et compétitions.</p>
+
+      <div className="flex justify-center my-8">
+        <Image
+          src="/nature.jpg"
+          alt="Nature"
+          width={900}
+          height={600}
+          className="rounded-xl"
+        />
+      </div>
+    </div>
   );
 }
